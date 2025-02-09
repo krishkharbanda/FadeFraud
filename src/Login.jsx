ed801b1cc06391b5f2c8b4911d732d4d99e4fca2
Anushka <akondur18@gmail.com>
2025-02-08 20:23:13 -0500

    updated login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
-import './App.css'; // Import the same CSS file
+import { Link } from 'react-router-dom';
+import './Login.css'; 
+
 
 function Login() {
   return (
     <div className="app">
       <main className="homepage">
+        <h1 className="homepage-title">Log In</h1>
         <form className="login-form">
-          <input
-            type="email"
-            placeholder="Email"
-            className="login-input"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="login-input"
-          />
-          <button type="submit" className="login-button">
-            Log in
-          </button>
+          <input type="email" placeholder="Email" className="login-input" />
+          <input type="password" placeholder="Password" className="login-input" />
+          <button type="submit" className="login-button">Log in</button>
         </form>
         <p className="signup-text">
-          Do not have an account? <Link to="/signup" className="signup-link">Sign up</Link>
+          Don't have an account? <Link to="/signup" className="signup-link">Sign up</Link>
         </p>
       </main>
     </div>
