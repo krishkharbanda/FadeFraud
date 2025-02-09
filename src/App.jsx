7cf318a320aaf2e91248135304c3a06b48ffcbaa
srimathivadivel <srimathi.vadivel@gmail.com>
2025-02-08 19:39:30 -0500

    switched to react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <main className="homepage">
+        <h1 className="homepage-title">FadeFraud</h1>
+        <a href="/login" className="login-button">
+          Log in
+        </a>
+      </main>
+    </div>
+  );
+}
+
+export default App;
